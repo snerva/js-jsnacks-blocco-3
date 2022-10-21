28f6189da9300252876a9c3f7669dccddd3cfad0
snerva <selenenerva@live.it>
2022-10-21 12:23:12 +0200

    jsnack2

---
+++ snack_2/assets/main.js
@@ -0,0 +1,11 @@
+//A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l’iniziale maiuscola.
+
+const words = ['qui', 'QUO', 'Qua'];
+
+
+words.forEach((element) => {
+    if(element !== element.toUpperCase){
+        element.toUpperCase
+    }
+})
+
