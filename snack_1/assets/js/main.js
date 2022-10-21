79d9802f5e60d67bf58e1bc1ebf41490f70ee125
snerva <selenenerva@live.it>
2022-10-21 11:41:36 +0200

    cars

---
+++ snack_1/assets/js/main.js
@@ -0,0 +1,22 @@
+/*Crea un array composto da 10 automobili.
+Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+Infine stampa separatamente i 3 array.*/
+
+const cars =[
+    {marca: bmw, modello: b, alimentazione: benzina },
+    {marca: audi, modello: c, alimentazione: diesel },
+    {marca: fors, modello: d, alimentazione: gpl },
+    {marca: fiat, modello: b, alimentazione: metano },
+    {marca: bmw, modello: b, alimentazione: elettrico },
+    {marca: audi, modello: c, alimentazione: diesel },
+    {marca: fors, modello: d, alimentazione: gpl },
+    {marca: fiat, modello: b, alimentazione: metano },
+    {marca: fors, modello: d, alimentazione: gpl },
+    {marca: fiat, modello: b, alimentazione: metano },
+]
+
+//dividi in 3 array per ogni proprietá
+const marcaCar = cars.filter (marca =>{
+    
+})
